94f3156f96eed2a99d9ced8cb40258e76c233ece
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-10-25 03:18:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let test = document.querySelector("#name");
+let time = document.querySelector("#delay");
+let submitBtn = document.querySelector("#btn");
+let output = document.querySelector("#output");
+
+submitBtn.addEventListener("click", setTimeout(()=>{
+	output.innerText = `${test}`;
+}, 1000))
+
